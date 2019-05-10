e69e666134631b5e2d0066d4d466f568c3600e36
FelixFonteneau <ffonteneau@insa-lyon.fr>
2019-05-10 08:33:41 +0200

    Visual

---
+++ projetVisualStudio/projetVisualStudio/projetVisualStudio.cpp
@@ -0,0 +1,12 @@
+// projetVisualStudio.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+using namespace std;
+
+int main()
+{
+	cout << "Hello World !";
+    return 0;
+}
+
