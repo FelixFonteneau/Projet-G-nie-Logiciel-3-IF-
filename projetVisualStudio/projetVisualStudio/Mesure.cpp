715161627ebdb6634c38adec30b68bed82193c49
FelixFonteneau <ffonteneau@insa-lyon.fr>
2019-05-10 10:07:22 +0200

    fin conception

---
+++ projetVisualStudio/projetVisualStudio/Mesure.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Mesure.h"
+
+
+Mesure::Mesure()
+{
+}
+
+
+Mesure::~Mesure()
+{
+}
---
+++ projetVisualStudio/projetVisualStudio/Mesure.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+class Mesure abstract
+{
+public:
+	Mesure();
+	~Mesure();
+
+	static string description;
+private:
+	double valeur;
+	 
+};
+
