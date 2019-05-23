cdfbcce27db24f4b78729dd349bdb88eae539d0c
FelixFonteneau <felix.fonteneau@hotmail.com>
2019-05-23 11:29:34 +0200

    fv

--- mesure/Mesure.cpp
+++ mesure/Mesure.cpp
@@ -16,6 +16,7 @@ using namespace std;
 
 //------------------------------------------------------ Include personnel
 #include "Mesure.h"
+#include "Moment.h"
 
 //------------------------------------------------------------- Constantes
 
--- mesure/testMoment.cpp
+++ mesure/testMoment.cpp
@@ -5,13 +5,12 @@
 using namespace std;
 
 #include "Moment.h"
-#include "Mesure.h"
-#include "MesureNO2.h"
+//#include "Mesure.h"
 
 int main()
 {
 
-  /*
+  
   Moment m1 ("2016-01-01T00:01:20.609000");
   Moment m2 ("2016-01-01T00:01:20.6090000");
   Moment m3(23,10,2019,01,01,02);
@@ -25,11 +24,8 @@ int main()
   cout << "m1 > m2 ? : " << a << endl;
   cout << "m1 < m2 ? : " << b << endl;
   cout << "m1 = m2 ? : " << c << endl;
-*/
 
-  Moment m1 ("2016-01-01T00:01:20.609000");
-  Moment m2 ("2016-01-01T00:01:20.6090000");
-  Moment m3(23,10,2019,01,01,02);
+
 
   //Mesure mesure(100,m1,"bla","pute/s");
   //cout << mesure << endl;
